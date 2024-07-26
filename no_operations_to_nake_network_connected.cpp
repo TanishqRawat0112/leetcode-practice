f5846fdfd0619d4c97272d086ba51dd6c06b6529
Tanishq Rawat <98083745+TanishqRawat0112@users.noreply.github.com>
2024-07-27 00:48:10 +0530

    Create no_operations_to_nake_network_connected.cpp

---
+++ no_operations_to_nake_network_connected.cpp
@@ -0,0 +1,97 @@
+// There are n computers numbered from 0 to n - 1 connected by ethernet cables connections forming a network where connections[i] = [ai, bi] represents a connection between computers ai and bi. Any computer can reach any other computer directly or indirectly through the network.
+
+// You are given an initial computer network connections. You can extract certain cables between two directly connected computers, and place them between any pair of disconnected computers to make them directly connected.
+
+// Return the minimum number of times you need to do this in order to make all the computers connected. If it is not possible, return -1.
+
+ 
+
+// Example 1:
+
+// Input: n = 4, connections = [[0,1],[0,2],[1,2]]
+// Output: 1
+// Explanation: Remove cable between computer 1 and 2 and place between computers 1 and 3.
+// Example 2:
+
+// Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2],[1,3]]
+// Output: 2
+// Example 3:
+
+// Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2]]
+// Output: -1
+// Explanation: There are not enough cables.
+ 
+
+// Constraints:
+
+// 1 <= n <= 105
+// 1 <= connections.length <= min(n * (n - 1) / 2, 105)
+// connections[i].length == 2
+// 0 <= ai, bi < n
+// ai != bi
+// There are no repeated connections.
+// No two computers are connected by more than one cable.
+
+class DisjointSet{
+    vector<int>size,parent;
+    public:
+        DisjointSet(int n){
+            for(int i=0;i<=n;i++){
+                size.push_back(1);
+                parent.push_back(i);
+            }
+        }
+    
+        int findParent(int node){
+            if(parent[node]==node){
+                return node;
+            }
+            return parent[node]=findParent(parent[node]);
+        }
+
+        void unionBySize(int u,int v){
+            int ulp_u=findParent(u);
+            int ulp_v=findParent(v);
+            if(ulp_u==ulp_v){
+                size[ulp_u]++;
+                return;
+            }
+            if(size[ulp_u]<size[ulp_v]){
+                parent[ulp_u]=ulp_v;
+                size[ulp_v]+=size[ulp_u];
+            }
+            else{
+                parent[ulp_v]=ulp_u;
+                size[ulp_u]+=size[ulp_v];
+            }
+        }
+};
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& edges) {
+        int m=edges.size();
+        DisjointSet ds(n);
+        int extraEdges=0;
+        int usedEdges=0;
+        for(int i=0;i<m;i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            if(ds.findParent(u)==ds.findParent(v)){
+                extraEdges++;
+            }
+            else{
+                ds.unionBySize(u,v);
+                usedEdges++;
+            }
+        }
+        if(usedEdges + extraEdges == n-1){
+            return extraEdges;
+        }
+        else if(usedEdges + extraEdges > n-1){
+            return n-1-usedEdges;
+        }
+        else{
+            return -1;
+        }
+    }
+};
